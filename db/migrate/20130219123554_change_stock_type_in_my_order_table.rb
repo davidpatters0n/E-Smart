514512a89796c1cc0454fdb9ac8751254d165b87
dj <dj@dj-VirtualBox.(none)>
2013-03-07 03:26:00 +0000

    Heroku fix v3

--- db/migrate/20130219123554_change_stock_type_in_my_order_table.rb
+++
@@ -1,9 +0,0 @@
-class ChangeStockTypeInMyOrderTable < ActiveRecord::Migration
-  def up
-    change_column :products, :stock, :integer
-  end
-
-  def self.down
-    change_column :products, :stock, :string
-  end
-end
